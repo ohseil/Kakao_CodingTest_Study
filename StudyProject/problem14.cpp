5339c216c919c401604020a14ccd277a4177638a
ohseil <sioh0612@gmail.com>
2020-07-25 15:37:30 +0900

    problem14 solved.

--- StudyProject/problem14.cpp
+++ StudyProject/problem14.cpp
@@ -1,153 +1,116 @@
 #include "problem14.h"
 
-bool checkBound(int index, int size) { return (0 <= index && index < size); }
+namespace KakaoBlind2019 {
 
-vector<vector<pair<int,int>>> checkBlock(int row, int col, int bNum, vector<vector<int>> board, int size) {
+	namespace problem14 {
 
-	// 되는 블럭의 모양을 확인해서
-	// 채워야 하는 블럭 두개를 반환.
+		int solution(vector<vector<int>> board) {
 
-	vector<vector<pair<int, int>>> result;
-	int minR, maxR, minC, maxC;
+			int answer = 0;
+			int size = board.size();
 
-	if (checkBound(row - 1, size) == false)
-		minR = row;
-	else if (checkBound(row - 2, size) == false)
-		minR = row - 1;
-	else
-		minR = row - 2;
+			int erased = 1;
 
-	if (checkBound(row + 1, size) == false)
-		maxR = row;
-	else if (checkBound(row + 2, size) == false)
-		maxR = row + 1;
-	else
-		maxR = row + 2;
+			// 각 열마다 다음에 어디행부터 채워야하는지 대한 정보
+			int* pos = new int[size];
+			for (int i = 0; i < size; i++)
+				pos[i] = 0;
 
-	minC = col;
+			// 지워진 블럭이 없을때까지 진행
+			while (erased != 0) {
 
-	if (checkBound(col + 1, size) == false)
-		maxC = col;
-	else if (checkBound(col + 1, size) == false)
-		maxC = col + 1;
-	else
-		maxC = col + 2;
+				erased = 0;
 
-	vector<pair<int, int>> block;
+				// 각 열마다 위에서부터 걸리는데까지 채우기
+				for (int i = 0; i < size; i++) {
 
-	for (int r = minR; r <= maxR; r++) {
-		for (int c = minC; c <= maxC; c++) {
-			if (board[r][c] == bNum)
-				block.push_back(make_pair<int, int>((int)r, (int)c));
-		}
-	}
+					for (int j = pos[i]; j < size; j++) {
+						if (board[j][i] == 0) {
+							board[j][i] = -1;
+						}
+						else if (board[j][i] != -1) {
+							// 블럭 존재
+							break;
+						}
 
-	int minx = block[0].first, miny = block[0].second, maxx = block[0].first, maxy = block[0].second;
-
-	for (auto a : block) {
-		if (minx > a.first)
-			minx = a.first;
-		if (miny > a.second)
-			miny = a.second;
-		if (maxx < a.first)
-			maxx = a.first;
-		if (maxy < a.second)
-			maxy = a.second;
-	}
-
-	vector<pair<int, int>> nonBlock;
-
-	for (int x = minx; x <= maxx; x++)
-		for (int y = miny; y <= maxy; y++)
-			if (board[x][y] != bNum)
-				nonBlock.push_back(make_pair<int, int>((int)x, (int)y));
-
-	result.push_back(block);
-	result.push_back(nonBlock);
-
-	return result;
-}
-
-int solution(vector<vector<int>> board) {
-	
-	int answer = 0;
-	int size = board.size();
-
-	vector<int> erased;
-	
-	for (int row = 0; row < size; row++) {
-
-		for (int col = 0; col < size; col++) {
-
-			if (board[row][col] != 0) {
-				// 블럭조각 존재
-				int bNum = board[row][col]; // block 번호
-
-				if (checkBound(col + 1, size) && board[row][col + 1] < size && board[row][col + 1] == bNum) {
-					// 안되는 블럭
-
+						pos[i] = j + 1;
+					}
 				}
-				else {
-					if ((checkBound(col - 1, size) && board[row][col - 1] >= 0 && board[row][col - 1] == bNum)
-						&& !(checkBound(col + 1, size) && board[row][col + 1] < size && board[row][col + 1] == bNum)) {
-						// 안되는 블럭
-					}
-					else if (!(checkBound(col - 1, size) && board[row][col - 1] >= 0 && board[row][col - 1] == bNum)
-						&& (checkBound(col + 1, size) && board[row][col + 1] < size && board[row][col + 1] == bNum)) {
-						// 안되는 블럭
 
-					}
-					else {
-						// 되는 블럭
-						// 채워야 하는 열이 비활성화된 열인지 확인.
 
-						vector<vector<pair<int, int>>> checkResult = checkBlock(row, col, bNum, board, size);
+				// 지워질 블럭이 있는지 확인하고 지우기.
+				// 2x3 사각형 모양으로 확인
+				for (int i = 0; i < size - 1; i++) {
+					for (int j = 0; j < size - 2; j++) {
 
-						bool isErase = true;
-
-						for (auto nonblock : checkResult[1]) {
-							int nbRow = nonblock.first;
-							int nbCol = nonblock.second;
-
-							for (int r = 0; r <= nbRow; r++) {
-								if (board[r][nbCol] != 0) {
-									isErase = false;
+						int blockNum = -1;
+						set<int> checkBlockCount;
+						bool canErase = true;
+						vector<pair<int, int>> v;
+						for (int x = i; x <= i + 1; x++) {
+							for (int y = j; y <= j + 2; y++) {
+								if (board[x][y] == 0) {
+									canErase = false;
 									break;
+								}
+								else if (board[x][y] != -1) {
+
+									blockNum = board[x][y];
+									checkBlockCount.insert(blockNum);
+									v.push_back(make_pair<int, int>((int)x, (int)y));
 								}
 							}
 						}
 
-						if (isErase == true) {
-							// 블럭 지우고 지워진 블럭번호 기록.
-							for (auto block : checkResult[0]) {
-								int bRow = block.first;
-								int bCol = block.second;
-								board[bRow][bCol] = 0;
-								erased.push_back(bNum);
+						if (canErase == true && blockNum != -1 && v.size() == 4 && checkBlockCount.size() == 1) {
+							// 지울 수 있는 블럭 지우기.
+							for (auto a : v) {
+								board[a.first][a.second] = 0;
 							}
+							erased++;
+							answer++;
 						}
 
 					}
 				}
 
+				// 3x2 사각형 모양으로 확인
+				for (int i = 0; i < size - 2; i++) {
+					for (int j = 0; j < size - 1; j++) {
+
+						int blockNum = -1;
+						set<int> checkBlockCount;
+						bool canErase = true;
+						vector<pair<int, int>> v;
+						for (int x = i; x <= i + 2; x++) {
+							for (int y = j; y <= j + 1; y++) {
+								if (board[x][y] == 0) {
+									canErase = false;
+									break;
+								}
+								else if (board[x][y] != -1) {
+
+									blockNum = board[x][y];
+									checkBlockCount.insert(blockNum);
+									v.push_back(make_pair<int, int>((int)x, (int)y));
+								}
+							}
+						}
+
+						if (canErase == true && blockNum != -1 && v.size() == 4 && checkBlockCount.size() == 1) {
+							// 지울 수 있는 블럭 지우기.
+							for (auto a : v) {
+								board[a.first][a.second] = 0;
+							}
+							erased++;
+							answer++;
+						}
+					}
+				}
 			}
 
-			for (auto a : board) {
-				for (auto aa : a)
-					cout << aa << " ";
-				cout << endl;
-			}
+			return answer;
 		}
-	}
-	
-	answer = erased.size();
-
-	return answer;
-}
-
-namespace KakaoBlind2019 {
-
-	namespace problem14 {
 
 		void execute() {
 
